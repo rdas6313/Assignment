a4dde653e8f1924ff586393dce284e634449c624
Raja Das <rdas6313@gmail.com>
2024-11-13 01:41:58 +0530

    changed lookup parameter

--- invoice_assignment/views.py
+++ invoice_assignment/views.py
@@ -9,7 +9,8 @@ from rest_framework.exceptions import MethodNotAllowed
 class InvoiceViewSet(viewsets.ModelViewSet):
     queryset = Invoice.objects.all()
     serializer_class = InvoiceSerializer
-    http_method_names = ['get', 'post', 'put']
+    http_method_names = ['get', 'post', 'put', 'head', 'option']
+    lookup_field = 'invoice_number'
 
     def list(self, request, *args, **kwargs):
         raise MethodNotAllowed('GET')
