08018e00e180d6099e19e41681dcc42fcfda22ea
Raja Das <rdas6313@gmail.com>
2024-11-13 01:01:36 +0530

    create admin for invoice

--- invoice_assignment/admin.py
+++ invoice_assignment/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+
+@admin.register(Invoice)
+class InvoiceAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(InvoiceDetail)
+class InvoiceDetailAdmin(admin.ModelAdmin):
+    pass
